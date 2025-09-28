724a2cc5d6dc68ed119d7278ffa67fd6caec7dcf
Heer Makwana <87268149+HeerMakwana@users.noreply.github.com>
2025-09-28 23:37:50 +0530

    Delete volto-multicolumn-dnd directory

--- volto-multicolumn-dnd/src/actions/multicolumnActions.js
+++
@@ -1,8 +0,0 @@
-export const MOVE_BLOCK = 'MULTICOLUMN/MOVE_BLOCK';
-
-export function moveBlock({ blockId, from, to, rowBlock }) {
-  return {
-    type: MOVE_BLOCK,
-    payload: { blockId, from, to, rowBlock },
-  };
-}
